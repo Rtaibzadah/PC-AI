75c9cb6f297a60312e744efee689fada53e7b394
roman <roman@Romans-MacBook-Air-2.local>
2024-09-24 15:48:36 +0100

    hello guys

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+import sys  # for system-specific parameters and functions
+import os   # for operating system dependent functionality
+
+def main():
+    print("Python version")
+    print("Hello, world!")
+    # Add your main code logic here
+
+if __name__ == "__main__":
+    main()
